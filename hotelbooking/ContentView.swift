37235ceb98d58521f1c3548f9ce2abbd947f6d42
HemanthKiranPolu <hemanthreddypolu@gmail.com>
2026-01-09 11:55:10 -0800

    Initial Commit

---
+++ hotelbooking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hotelbooking
+//
+//  Created by hemanth kiran Polu on 1/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hotelbooking/hotelbookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  hotelbookingApp.swift
+//  hotelbooking
+//
+//  Created by hemanth kiran Polu on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct hotelbookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
